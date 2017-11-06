d4065312635fd9b57360976de65c02769ee407e1
DevYves <yfreypons@gmail.com>
2017-11-06 20:27:01 +0000

    changed position of var decleration and function call

--- printing-with-parameters/printing-with-parameters.js
+++ printing-with-parameters/printing-with-parameters.js
@@ -3,37 +3,27 @@
 
 
 const https = require('https');
-
 var requestOptions = {
     host: 'sytantris.github.io',
     path: '/http-examples/step1.html'
   };
+getAndPrintHTMLChunks(requestOptions);
 
 
 function getAndPrintHTMLChunks (options) {
-
-https.get(options, function (response) {
-
-
-var responseBodyString = "";
-  // set encoding of received data to UTF-8
-  response.setEncoding('utf8');
-
-  // the callback is invoked when a `data` chunk is received
-  response.on('data', function (chunk) {
-
+  https.get(options, function (response) {
+    var responseBodyString = "";
+    // set encoding of received data to UTF-8
+    response.setEncoding('utf8');
+    // the callback is invoked when a `data` chunk is received
+    response.on('data', function (chunk) {
     responseBodyString += chunk;
-      // (the `end` of the stream)
-
+    // (the `end` of the stream)
+    });
+    response.on('end', function() {
+    console.log(responseBodyString);
+    });
   });
-  response.on('end', function() {
-      console.log(responseBodyString);
-
-  });
-
-
-
-});
-
 }
-getAndPrintHTMLChunks(requestOptions);
+
+
